719b9b4994ef1494b0388f4caf5ca66067676815
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-11-19 19:07:59 -0300

    FEAT: criação do endpoint login OAuth2

--- src/app/controllers/__init__.py
+++ src/app/controllers/__init__.py
@@ -8,12 +8,26 @@ from google_auth_oauthlib.flow import Flow
 
 login = Blueprint("login", __name__,  url_prefix="/login")
 
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file="src\\app\\utils\\client_secret.json",
+    scopes=[
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid",
+    ],
+    redirect_uri="http://localhost:5000/login/callback",
+)
+
 
 @login.route("/auth/google", methods=["POST"])
 def auth_google():
-    
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+
     return Response(
-          response=json.dumps({"url": "Funcionando"}),
-          status=200,
-          mimetype="application/json",
+		response=json.dumps({"url": authorization_url}),
+		status=200,
+		mimetype="application/json",
         )
