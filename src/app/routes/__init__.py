da2b5cd284e3e8954c2953860dff444514a4d15d
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-11-20 15:33:39 -0300

    FEAT: endpoint callback retornando os dados e o redirect para o front

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from src.app.controllers import login
+from src.app.controllers.login import login
+from src.app.controllers.data import data
 
 
 def routes(app: Flask):
     app.register_blueprint(login)
+    app.register_blueprint(data)
